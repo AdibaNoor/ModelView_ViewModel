baf0582a993e4b10bce71395781369365b104a26
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-28 19:20:49 +0530

    added user defined exception classes

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,29 @@
+
+
+class AppExceptions implements Exception{
+  final _message;
+  final _prefix;
+
+  AppExceptions([this._message, this._prefix]);
+
+  String toString(){
+    return '$_prefix$_message';
+  }
+}
+
+
+class FetchDataException extends AppExceptions{
+  FetchDataException([String? message]): super(message, 'Error During Communication');
+}
+
+class BadRequestException extends AppExceptions{
+  BadRequestException([String? message]): super(message, 'Invalid Request');
+}
+
+class UnauthorisedException extends AppExceptions{
+  UnauthorisedException([String? message]): super(message, 'Unauthorised Request');
+}
+
+class InvalidInputException extends AppExceptions{
+  InvalidInputException([String? message]): super(message, 'Invalid Input');
+}
